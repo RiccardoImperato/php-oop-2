9ac47c7d6ed4178268281d63c0e9862478d16a99
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-16 16:55:42 +0100

    feat: font Montserrat e stile card prodotti

--- index.php
+++ index.php
@@ -55,57 +55,76 @@ $lettino = new Type('Lettino', 20, 'Gatto', 'Cuccia');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Petshop</title>
+    <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Font Montserrat -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Style -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-    <ul>
-        <li>
-            <figure>
-                <img src="https://www.cuccia-per-cani.com/wp-content/uploads/2018/12/31uaaHXJYGL.jpg" alt="">
-            </figure>
-            <h3><?php echo $osso_smart->name; ?></h3>
-            <p>
-                Categoria: <?php echo $osso_smart->get_icon() ?>
-            </p>
-            </p>
-            <p>
-                Tipo: <?php echo $osso_smart->type; ?>
-            </p>
-            <p>
-                Prezzo: <?php echo $osso_smart->price; ?> £
-            </p>
-        </li>
-        <li>
-            <figure>
-                <img src="https://www.my-personaltrainer.it/2021/04/13/cibo-secco-per-il-gatto-orig.jpeg" alt="">
-            </figure>
-            <h3><?php echo $crocchette->name; ?></h3>
-            <p>
-                Categoria: <?php echo $crocchette->get_icon() ?>
-            </p>
-            <p>
-                Tipo: <?php echo $crocchette->type; ?>
-            </p>
-            <p>
-                Prezzo: <?php echo $crocchette->price; ?> £
-            </p>
-        </li>
-        <li>
-            <figure>
-                <img src="https://img.kwcdn.com/product/1e23310c6c/44d3d2f9-fa82-454f-bd9e-b0da5cbf9c39_800x800.jpeg?imageView2/2/w/800/q/70" alt="">
-            </figure>
-            <h3><?php echo $lettino->name; ?></h3>
-            Categoria: <?php echo $lettino->get_icon() ?>
-            <p>
-                Tipo: <?php echo $lettino->type; ?>
-            </p>
-            <p>
-                Prezzo: <?php echo $lettino->price; ?> £
-            </p>
-        </li>
-    </ul>
+    <div class="container">
+        <ul>
+            <li class="card">
+                <figure>
+                    <img src="https://www.cuccia-per-cani.com/wp-content/uploads/2018/12/31uaaHXJYGL.jpg" alt="">
+                </figure>
+                <div class="info">
+                    <h3><?php echo $osso_smart->name; ?></h3>
+                    <div>
+                        Categoria: <?php echo $osso_smart->get_icon() ?>
+                    </div>
+                    <div>
+                        Tipo: <?php echo $osso_smart->type; ?>
+                    </div>
+                    <div>
+                        Prezzo: <?php echo $osso_smart->price; ?>
+                        <i class="fa-solid fa-euro-sign"></i>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <figure>
+                    <img src="https://www.my-personaltrainer.it/2021/04/13/cibo-secco-per-il-gatto-orig.jpeg" alt="">
+                </figure>
+                <div class="info">
+                    <h3><?php echo $crocchette->name; ?></h3>
+                    <div>
+                        Categoria: <?php echo $crocchette->get_icon() ?>
+                    </div>
+                    <div>
+                        Tipo: <?php echo $crocchette->type; ?>
+                    </div>
+                    <div>
+                        Prezzo: <?php echo $crocchette->price; ?>
+                        <i class="fa-solid fa-euro-sign"></i>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <figure>
+                    <img src="https://img.kwcdn.com/product/1e23310c6c/44d3d2f9-fa82-454f-bd9e-b0da5cbf9c39_800x800.jpeg?imageView2/2/w/800/q/70" alt="">
+                </figure>
+                <div class="info">
+                    <h3><?php echo $lettino->name; ?></h3>
+                    <div>
+                        Categoria: <?php echo $lettino->get_icon() ?>
+                    </div>
+                    <div>
+                        Tipo: <?php echo $lettino->type; ?>
+                    </div>
+                    <div>
+                        Prezzo: <?php echo $lettino->price; ?>
+                        <i class="fa-solid fa-euro-sign"></i>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </body>
 
 </html>
